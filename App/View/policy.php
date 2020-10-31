0a5acb63ad5d83dbbc21d2b611f16d6be25ef4d6
clara <claramvnbrg@gmail.com>
2020-10-31 16:53:36 +0100

    Pages legals mentions and privacy policy

---
+++ App/View/policy.php
@@ -0,0 +1,156 @@
+<!-- Titre site -->
+<?php $title = 'Politique de confidentialité'; ?>
+
+<!-- Content -->
+<?php ob_start(); ?>
+
+<div class="content">
+
+    <!-- Retour accueil -->
+    <div class="links"><a href="index.php">Accueil</a> <span class="red"><i class=" fas
+                    fa-chevron-right"></i><i class="fas fa-chevron-right"></i></span> Politique de confidentialité
+    </div>
+
+    <div id="privacyPolicy">
+        <h1>[ <strong>POLITIQUE DE CONFIDENTIALITE</strong> ]</h1><br /><br />
+
+        <p>Dans le cadre de la visite du site <strong>claramvn.fr</strong>, <strong>le propriétaire</strong> est
+            susceptible
+            de collecter et traiter des données à caractère personnel de tout
+            utilisateur du Site (ci-après le(s) « Utilisateur(s) »). Toute information se rapportant à une personne
+            physique
+            susceptible d’être identifiée directement ou indirectement (par exemple les nom, prénom, identifiant,
+            adresse
+            électronique, données de localisation etc.) est une donnée à caractère personnel (ci-après « Donnée(s)
+            Personnelle(s) » ou « Donnée(s) »).
+
+            La présente charte Données Personnelles (ci-après la « Charte ») précise les modalités des traitements dont
+            la
+            Société est responsable, conformément à la réglementation en vigueur. La Charte vise à informer les
+            Utilisateurs
+            des pratiques de collecte, d’utilisation, de conservation et de partage des Données Personnelles.
+
+            <strong>Le propriétaire</strong> se réserve le droit de modifier, à tout moment et sans préavis, tout ou
+            partie
+            des règles
+            relatives à la protection des Données Personnelles. Les modifications, datées, entrent en vigueur et sont
+            opposables aux Utilisateurs dès leur mise en ligne sur le Site. </p>
+
+
+
+        <p><strong>1. Responsable du traitement des données personnelles</strong></p>
+
+        <p>Les données sont collectées par <strong>le propriétaire</strong>. Les traitements automatisés de
+            vos données
+            personnelles sont gérés en conformité avec la réglementation en vigueur.</p>
+
+
+        <p><strong>2.1. Collecte et utilisation des données</strong></p>
+
+        <p>Certaines Données Personnelles de l’Utilisateur sont susceptibles d’être recueillies lorsque l’Utilisateur :
+            - s’inscrit sur le Site quelle que soit la finalité (par exemple création de compte par adresse mail) ;
+            - adresse une demande au propriétaire via le formulaire de contact du Site ;
+
+            Ces Données peuvent être notamment selon les rubriques :
+            - les données d’identification et coordonnées de l’Utilisateur (nom, prénom, adresse email) ;
+            - les données de connexion de l’Utilisateur à un compte (identifiant, mot de passe) ;
+
+            Les Données marquées d'un astérisque revêtent un caractère obligatoire. Le défaut de réponse sur l’une de
+            ces
+            informations ne permettra pas d’adresser à l’Utilisateur les informations sollicitées, de traiter sa demande
+            ou
+            de
+            lui donner accès audit service.</p>
+
+
+        <p><strong>2.1.2 Finalité du traitement des données personnelles</strong></p>
+
+        <p><strong>Le propriétaire</strong> collecte les Données Personnelles de l’Utilisateur afin de :
+            - communiquer avec l’Utilisateur, notamment pour :
+            • traiter ses demandes via les formulaires de contact ;</p>
+
+
+
+        <p><strong>3. Destinataires des données personnelles</strong></p>
+
+        <p>Les Données Personnelles collectées par <strong>le propriétaire</strong> sont destinées à être utilisées par
+            cette dernière.
+
+            Les Données des Utilisateurs pourront enfin être communiquées à toutes autorités judiciaires ou
+            administratives
+            qui
+            en feraient la demande.</p>
+
+
+        <p><strong>4. Durée de conservation des données personnelles</strong></p>
+
+        <p><strong>Le propriétaire</strong> conserve les Données Personnelles pour une durée qui n'excède pas la durée
+            nécessaire d'une session.
+        </p>
+
+
+        <p><strong>5. Sécurité et protection des données personnelles</strong></p>
+
+        <p>Les Données Personnelles collectées par <strong>le propriétaire</strong> sont conservées dans un
+            environnement
+            sécurisé. Elles
+            sont
+            hébergées auprès du <strong>prestataire</strong> désigné dans les <a
+                href="index.php/action=mentionsLegals">Mentions Légales</a> du Site
+            <strong>Le propriétaire</strong> prend toutes les mesures physiques, techniques et organisationnelles
+            appropriées pour assurer la
+            sécurité
+            et la confidentialité des Données Personnelles en vue de les protéger contre toute perte, destruction
+            accidentelle,
+            altération et accès non autorisé.
+            Cependant, <strong>le propriétaire</strong> ne maîtrise pas l’ensemble des risques liés à l’Internet. A ce
+            titre, l’Utilisateur
+            consulte
+            le Site à ses risques et périls, sans que la Société ne puisse être tenue pour responsable d’une faille de
+            sécurité
+            qu’elle ne serait pas en mesure de pallier.</p>
+
+
+        <p><strong>6. Droits des utilisateurs</strong></p>
+
+        <p><strong>Le propriétaire</strong> garantit aux Utilisateurs le respect des droits prévus par la loi 6 janvier
+            1978
+            modifiée dite «
+            Informatique et Libertés » et par le règlement (UE) du 27 avril 2016 relatif à la protection des Données
+            Personnelles (ci-après « RGPD »), particulièrement le droit pour les Utilisateurs de décider et de contrôler
+            les
+            usages qui sont faits de leurs Données Personnelles.
+            Il est rappelé que, conformément auxdites réglementations, les Utilisateurs disposent d’un droit d’accès, de
+            rectification, d’effacement des Données Personnelles les concernant, mais également un droit à la limitation
+            du
+            traitement.
+            Dans la mesure où les traitements mis en œuvre dans le cadre de l’exploitation du Site reposent sur le
+            consentement
+            des Utilisateurs, ceux-ci peuvent retirer leur consentement à tout moment.
+
+            En outre, en application de l’article 20 du RGPD sur le droit de portabilité des données, les Utilisateurs
+            peuvent
+            en toutes circonstances récupérer l'ensemble de leurs Données, dans un format structuré et les transmettre à
+            un
+            autre responsable de traitement.
+
+            Enfin, l’Utilisateur peut définir des directives post-mortem relatives à la conservation, à l’effacement et
+            à la
+            communication de ses Données Personnelles. L’Utilisateur peut modifier ou révoquer ses directives à tout
+            moment.
+
+            En cas de litige, l’Utilisateur peut introduire une réclamation auprès de l’autorité de contrôle.</p>
+
+
+        <p><strong>8. Loi applicable et recours</strong></p>
+
+        <p>La présente Charte est régie, interprétée et appliquée conformément au droit français.
+            En cas de litige portant sur le traitement de ses Données Personnelles dans le cadre de l’exploitation du
+            Site,
+            l’Utilisateur et <strong>le propriétaire</strong> s’engagent à rechercher préalablement et de bonne foi une
+            solution amiable.</p>
+    </div>
+</div>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('App/View/template.php');
