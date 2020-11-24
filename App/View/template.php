c647f2108ae9b53e5a8305512cdf7f388507ad87
clara <claramvnbrg@gmail.com>
2020-11-24 16:20:33 +0100

    adjustments links

--- App/View/template.php
+++ App/View/template.php
@@ -23,43 +23,60 @@
 
 <body>
 
-    <!-- Header --> 
+    <!-- HEADER --> 
     <header id="header">
         <div>
             <h1 id="librum"><a href="index.php">LIBRUMNIVORES</a></h1>
         </div>
         <div>
             <nav>
-                <ul>
-                    <li><a href="index.php?action=home#slogan">FONCTIONNALITÉS</a></li>
-                    <li><a href="index.php?action=connection">SE CONNECTER</a></li>
-                    <li><a href="index.php?action=registration">CRÉER UN COMPTE</a></li>
-                    <li><a id="btn_toggle_theme"><i class="fas fa-adjust"></i></a></li>
+                <ul class="nav">
+                    <?php
+                    if ($this->isLogged()) { 
+                    ?>
+                        <li class="nav-item"><a class="nav-link" >Bienvenue à toi <?php echo ' ' . $this->user['name_user'] . ' ';?> <span class="logged"><i class="fas fa-square"></i></span></a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php">ACCUEIL</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php?action=listBooks">BIBLIOTHÈQUE</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php?action=listWishBooks">SOUHAITS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php?action=listFavoritesBooks">FAVORIS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php?action=listLentBooks">PRÊTS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php?action=logout">DÉCONNECTION</a></li>
+                        <li class="nav-item"><a class="nav-link" id="btn_toggle_theme"><i class="fas fa-adjust"></i></a></li>
+                        <?php 
+                    } else {
+                    ?>
+                        <li class="nav-item"><a class="nav-link" href="index.php?action=home#slogan">FONCTIONNALITÉS</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php?action=connection">MON COMPTE</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php?action=register">CRÉER UN COMPTE</a></li>
+                        <li class="nav-item"><a class="nav-link" id="btn_toggle_theme"><i class="fas fa-adjust"></i></a></li>
+                    <?php
+                    }
+                    ?>
                 </ul>
             </nav> 
         </div>
     </header>
 
-    <!-- sections -->
+    <!-- SECTIONS -->
     <?= $content ?>
 
-    <!-- Contact --> 
+    <!-- CONTACT --> 
     <div id="contact">
         <p><a href="index.php?action=contact"><span class="red"><i class="fas fa-square"></i></span> CONTACTER LIBRUMNIVORES <span class="red"><i class="fas fa-square"></i></span></a></p>
         <p>Une question ? Un disfonctionnement ? Faites-nous en part !</p>
     </div>
-    <!-- Flèche scroll-top -->
+    <!-- ARROW GO TOP -->
     <p id="arrow"><a href="#"><i class="fas fa-arrow-up"></i></a></p>
-    <!-- Footer -->
+    <!-- FOOTER -->
     <footer>
         <p><a href="index.php?action=mentions">MENTIONS LÉGALES</a></p>
         <p> © COPYRIGHT 2020 - CLARA MORVAN ® - Tous droits réservés.</p>
         <p><a href="index.php?action=privacyPolicy">POLITIQUE DE CONFIDENTIALITÉ</a></p>
     </footer>
-    <!-- script -->
+    <!-- SCRIPT -->
     <script src="Public/js/theme.js"></script>
 
-    <!-- Bootstrap -->
+    <!-- BOOTSTRAP -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
