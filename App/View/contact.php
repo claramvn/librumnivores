fe490df1b4c8c6c3e5085fb1125f4f68b7d2a661
clara <claramvnbrg@gmail.com>
2020-11-24 16:17:51 +0100

    adjustments with script js

--- App/View/contact.php
+++ App/View/contact.php
@@ -1,9 +1,8 @@
 <?php
-// Titre site
 $title = 'Contact';
 ?>
 
-<!-- Content -->
+<!-- CONTENT -->
 <?php ob_start(); ?>
 
 <div class="content">
@@ -17,18 +16,19 @@ $title = 'Contact';
         <h1>Contact <span class="red"><i class="fas fa-square"></i></span></h1>
         <form action="index.php?action=contact" method="post">
             <div class="form-group">
-                <label for="user_name">NOM D'UTILISATEUR</label>
+                <label for="user_name">NOM D'UTILISATEUR<span class="asterisk">*</span></label>
                 <input type="text" class="form-control" id="user_name" name="user_name" value="<?= $name ?>" required>
             </div>
             <div class="form-group">
-                <label for="user_email">E-MAIL</label>
+                <label for="user_email">E-MAIL<span class="asterisk">*</span></label>
                 <input type="email" class="form-control" id="user_email" name="user_email" value="<?= $email ?>" required>
             </div>
             <div class="form-group">
-            <label for="user_message">MESSSAGE</label>
-            <textarea class="form-control" id="user_message" name="user_message" rows="3" required><?= $message ?></textarea>
+            <label for="user_message">MESSSAGE<span class="asterisk">*</span></label>
+            <textarea class="form-control" id="user_message" name="user_message" rows="3" minlength="15" required><?= $message ?></textarea>
             </div>
-            <input id="button_form" name="button_contact" type="submit" class="btn btn-primary" value="ENVOYER" />
+            <div id="mentions_required"><p><span class="asterisk">*</span> OBLIGATOIRE</div>
+            <input id="button_contact" name="button_contact" type="submit" class="btn btn-primary" value="ENVOYER" />
         </form>
 
         <!-- ERRORS -->
@@ -46,6 +46,8 @@ $title = 'Contact';
         ?>
     </div>
 </div>
+
+<script src="Public/js/contact.js"></script>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('App/View/template.php');
