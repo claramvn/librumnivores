9e0609df9d98475dc9e1c1da1f766ce4fb707f89
clara <claramvnbrg@gmail.com>
2020-11-24 16:06:30 +0100

    view error

---
+++ App/View/error.php
@@ -0,0 +1,14 @@
+<?php
+$title = 'Erreur';
+?>
+
+<!-- CONTENT -->
+<?php ob_start(); ?>
+
+<div class="content">
+<div id="error"><img src="Public/img/error.png" alt="Librumnivores - Page erreur" /></div>
+<div id="para_error"><p>Oups, il semblerait que la page que vous cherchez soit introuvable ...</p></div>
+</div>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('App/View/template.php');
