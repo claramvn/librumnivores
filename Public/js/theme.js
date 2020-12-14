decd7d946e628afdaf0e333a4b3dde37dc082f3f
clara <claramvnbrg@gmail.com>
2020-12-14 17:22:36 +0100

    updates on header : nav -> navbar bootstrap

--- Public/js/theme.js
+++ Public/js/theme.js
@@ -4,6 +4,7 @@ class Theme {
     constructor() {
         this.darkMode = sessionStorage.getItem("darkMode");
         this.header = document.getElementById('header');
+        this.nav = document.getElementById('navbarNav')
         this.links = document.querySelectorAll("a");
         this.btnToggle = document.getElementById("btn_toggle_theme");
     }
@@ -13,6 +14,8 @@ class Theme {
         document.body.classList.remove("lightMode");
         this.header.classList.add("darkMode");
         this.header.classList.remove("lightMode");
+        this.nav.classList.add("darkMode");
+        this.nav.classList.remove("lightMode");
 
         for (let i = 0; i < this.links.length; i++) {
             this.links[i].style.color = "white";
@@ -26,6 +29,8 @@ class Theme {
         document.body.classList.remove("darkMode");
         this.header.classList.add("lightMode");
         this.header.classList.remove("darkMode");
+        this.nav.classList.add("lightMode");
+        this.nav.classList.remove("darkMode");
 
         for (let i = 0; i < this.links.length; i++) {
             this.links[i].style.color = "black";
